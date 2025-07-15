882f7cd0166e09562f719cd03633ec8d79e9d79b
bezalel6 <bezalel3250@gmail.com>
2025-07-15 03:15:44 +0300

    Adds staggered animations to hover effect

--- PlayerComponent.tsx
+++ PlayerComponent.tsx
@@ -63,6 +63,7 @@ const SkipNext = Svg("M7.58 16.89l5.77-4.07c.56-.4.56-1.24 0-1.63L7.58 7.11C6.91
 const Repeat = Svg("M7 7h10v1.79c0 .45.54.67.85.35l2.79-2.79c.2-.2.2-.51 0-.71l-2.79-2.79c-.31-.31-.85-.09-.85.36V5H6c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1s1-.45 1-1V7zm10 10H7v-1.79c0-.45-.54-.67-.85-.35l-2.79 2.79c-.2.2-.2.51 0 .71l2.79 2.79c.31.31.85.09.85-.36V19h11c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1s-1 .45-1 1v3z", "repeat");
 const Shuffle = Svg("M10.59 9.17L6.12 4.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.46 4.46 1.42-1.4zm4.76-4.32l1.19 1.19L4.7 17.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L17.96 7.46l1.19 1.19c.31.31.85.09.85-.36V4.5c0-.28-.22-.5-.5-.5h-3.79c-.45 0-.67.54-.36.85zm-.52 8.56l-1.41 1.41 3.13 3.13-1.2 1.2c-.31.31-.09.85.36.85h3.79c.28 0 .5-.22.5-.5v-3.79c0-.45-.54-.67-.85-.35l-1.19 1.19-3.13-3.14z", "shuffle");
 const VolumeIcon = Svg("M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z", "volume");
+const DividerIcon = Svg("M6.5 17.5L17.5 6.5C18.33 5.67 18.33 4.33 17.5 3.5C16.67 2.67 15.33 2.67 14.5 3.5L3.5 14.5C2.67 15.33 2.67 16.67 3.5 17.5C4.33 18.33 5.67 18.33 6.5 17.5Z", "divider");
 
 function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
@@ -196,9 +197,11 @@ function WinampSeekBar() {
                     {msToHuman(statePosition)}
                 </span>
                 <span
-                    className={cl("progress-time")}
+                    className={cl("progress-time", "progress-divider")}
                     style={{ userSelect: "text" }}
-                >/</span>
+                >
+                    /
+                </span>
                 <span
                     className={cl("progress-time", "time-right")}
                     style={{ userSelect: "text" }}
